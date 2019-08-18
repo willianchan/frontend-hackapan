bc1da641fdd333a2b3c4d3a26f39c7ff1e3815c7
Chan <willian_chan_@hotmail.com>
2019-08-18 01:35:34 -0300

    adicionado tela de carros(preço)

--- carros.js
+++ carros.js
@@ -1,23 +1,57 @@
-$(document).ready(function(){
-    $('.limites').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: true,
-        variableWidth: true,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+$('.limites').slick({
+    rows: 2,
+    slidesPerRow: 2,
+    slidesToShow:1,
+    slidesToScroll:1,
+    mobileFirst: true,
+    arrows:false,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          rows: 3,
+          slidesPerRow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          rows: 3,
+          slidesPerRow: 4
+        }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          rows: 3,
+          slidesPerRow: 6
+        }
+      }
+    ]
+  });
+
+
+// $(document).ready(function(){
+//     $('.limites').slick({
+//         slidesToShow:2,
+//         slidesToScroll:2,
+//         autoplay: false,
+//         autoplaySpeed: 1500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: true,
+//         variableWidth: true,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// });
